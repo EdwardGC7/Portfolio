57f944510d5f4313cc516e141ad3f5b0821169a9
Osvaldo Gonzalez <gonzalezcampososvaldo@gmail.com>
2023-03-06 23:04:19 -0400

    projects section terminada

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -4,9 +4,9 @@ import '../styles/projectsCard.css'
 const ProjectsCard = (props) => {
   return (
     <div className='card_container'>
-      <div className='card_img_container'><img className='card_img' src={props.imgUrl} alt="Project img" /></div>
+      <div className='card_img_container'><img className='card_img' src={props.imgUrl} alt="Project img" /><div className='card_fade'><h2>{props.title}</h2><p>{props.description}</p></div></div>
+      
       <p>{props.title}</p>
-      <div className='card_fade'></div>
     </div>
   )
 }
