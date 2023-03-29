0a304fb772173a68f9bc05d10770e8af7fbde2a9
Osvaldo Gonzalez <gonzalezcampososvaldo@gmail.com>
2023-03-29 10:51:37 -0400

    deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://EdwardGC7.github.io/Portfolio/',
+  base:'/Portfolio/',
   server: {
     watch: {
         usePolling: true 
